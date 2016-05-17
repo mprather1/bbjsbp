f76a72450042a783bc24fe73235af34a4805a9a2
Michael Prather <mprather1@gmail.com>
2016-05-17 11:27:19 -0500

    listItemView

--- public/app.js
+++ public/app.js
@@ -1,5 +1,21 @@
-var Post = Backbone.Model.extend({});
+_.templateSettings = {
+interpolate: /\{\{(.+?)\}\}/g
+};
+
+var Post = Backbone.Model.extend({
+
+});
+
 var Posts = Backbone.Collection.extend({
   model: Post,
   url: "/posts"
+});
+
+var PostListView = Backbone.View.extend({
+  tagName: "li",
+  template: _.template("<a href='/posts/{{id}}'>{{title}}</a>"),
+  render: function() {
+    this.el.innerHTML = this.template(this.model.toJSON());
+    return this;
+  }
 });
